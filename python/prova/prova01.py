a20012e226e9db54972129a79ef45c3504a4dbe7
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2023-09-26 22:46:43 -0300

    prova 01 python
    prova 01 de python - Prova Revisão de Cond. e Repet.

---
+++ python/prova/prova01.py
@@ -0,0 +1,25 @@
+email = "thyago142536@gmail.com"
+senha = "123456"
+
+# Utilizando condição dentro do while
+while True:
+    email_user = input("Digite seu email: ")
+    senha_user = input("Digite sua senha: ")
+
+    if email_user == email and senha_user == senha:
+        print("Login realizado com sucesso!")
+        break
+    else:
+        print("Email ou senha incorretos!")
+
+
+# Utilizando somente while
+email_user = input("Digite seu email: ")
+senha_user = input("Digite sua senha: ")
+
+while senha_user != senha or email_user != email:
+   print("Senha ou email incorretos. Tente novamente.")
+   email_user = input("Digite a senha: ")
+   senha_user = input("Digite o email: ")
+
+print("Login realizado com sucesso!")
