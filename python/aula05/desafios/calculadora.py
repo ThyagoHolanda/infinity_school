3124282f10d0881a047f22708ece0fc0d4358bbb
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2023-10-14 21:51:19 -0300

    Update calculadora.py

--- python/aula05/desafios/calculadora.py
+++ python/aula05/desafios/calculadora.py
@@ -1,40 +1,55 @@
-def soma(num):
-    for i in range(len(num)):
-        if num[i] == "+":
-            num[i+1] = int(num[i-1]) + int(num[i+1])
-            
-    return num[-1]
-            
-def sub(num):
-    for i in range(len(num)):
-        if num[i] == "-":
-            num[i+1] = int(num[i-1]) - int(num[i+1])
-            
-    return num[-1]
+import datetime
 
-def mult(*num):
-    return int(num[0])*int(num[1])
+def ano_bissextos(ano):
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        return True
+    else:
+        return False
 
-def div(*num):
-    return int(num[0])/int(num[1])
 
-def expresao(e):
-    while len(e) > 1:
-        e
+def dias_mes(mes, bissextos=False):
+    quant_dias_mes = {
+        1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31,
+    }
+    if bissextos == True and mes == 2:
+        quant_dias_mes[2] = 29
+
+    return quant_dias_mes[mes]
+
+
+def verificar_data(dia, mes, ano):
+
+    if ano <= 0 or mes <= 0 or mes > 12 or dia <= 0 or dia > dias_mes(mes, ano_bissextos(ano)):
+        return False
+
+    return True
+
+def quantidade_ano_bissextos(ano_atual, ano):
+    acumulador = 0
+    for i in range(ano_atual-ano):
+        acumulador += 1 if ano_bissextos(ano) == True else 0
+        ano += 1
     
-    
-    
+    return acumulador
 
 
-s = "2/2/5/4"
-equ = []
-for i in s:            
-    equ.append(i)
+def conversao(dia, mes, ano):
+    data_atual = datetime.date.today()
+    ano_atual = data_atual.year
+    mes_atual = data_atual.month
+    dia_atual = data_atual.day
 
-print(equ.index("*"))
+    if verificar_data(dia, mes, ano) == True:
+        d_ano = ((ano_atual - ano) * 365) + quantidade_ano_bissextos(ano_atual, ano)
+        d_mes = (mes_atual - mes) * 30
+        d_dia = (dia_atual - dia) + 1
+        return d_dia + d_mes + d_ano
+    else:
+        return False
 
 
+dia = int(input("Dia: "))
+mes = int(input("Mês: "))
+ano = int(input("Ano: "))
 
-equacao = "5+6-1*2+5+6-5+7-2/2*5/4"
-res = 5+6-1*2+5+6-5+7-2/2*5/4
-
+print(conversao(dia, mes, ano))
