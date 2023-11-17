b9f5fca89226ea0c3a518af6c10e3e784a725b32
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2023-11-17 20:56:41 -0300

    Update ex02.py

--- python/aula12/ex02.py
+++ python/aula12/ex02.py
@@ -17,33 +17,89 @@ ele crie uma conta-correte ou conta poupança nova e, que consiga utilizar
 todas as funções.
 """
 
+
 class Contas:
-    def __init__(self, codigo, nome, saldo):
+    def __init__(self, codigo, nome, saldo, tipo):
         self.codigo = codigo
         self.nome = nome
         self.saldo = saldo
+        self.tipo = tipo
 
-    def verificar_informacoes():
-        pass
+        self.addconta()
 
-    def deposito(valor):
-        pass
+    def addconta(self):
+        global contas
+        global codigo
 
+        contas[codigo] = {
+            "titular": self.nome,
+            "saldo": self.saldo,
+            "tipo": self.tipo
+        }
+        codigo += 1
 
-class CC(Contas):
-    def __init__(self, codigo, nome, saldo):
-        super().__init__(codigo, nome, saldo)
+    @staticmethod
+    def verificar_informacoes(numero):
+        print(f"Titular: {contas[numero]['titular']}\n"
+              f"Saldo: {contas[numero]['saldo']}\n"
+              f"Tipo: {contas[numero]['tipo']}")
 
-
-    def sacar(valor):
-        self.saldo -= valor
-
-class CP(Contas):
-    def __init__(self, codigo, nome, saldo):
-        super().__init__(codigo, nome, saldo)
-
-    def juros(valor):
+    def deposito(self, valor):
         self.saldo += valor
 
 
+class CC(Contas):
+    def __init__(self, codigo, nome, saldo, tipo):
+        super().__init__(codigo, nome, saldo, tipo)
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
+
+class CP(Contas):
+    def __init__(self, codigo, nome, saldo, tipo):
+        super().__init__(codigo, nome, saldo, tipo)
+
+    def juros(self, valor):
+        self.saldo += valor
+
+
+print("Precione enter para começar")
+contas = {}
+codigo = 1
+while True:
+    print("1 - Criar Conta-Corrente\n"
+          "2 - Criar Conta-Poupança\n"
+          "3 - Sacar\n"
+          "4 - Depositar\n"
+          "5 - Verificar informações\n"
+          "0 - Sair")
+    escolha = int(input("Escolha uma das opções acima: "))
+
+    match escolha:
+        case 1:
+            nome = input("Informe o nome do titular da conta: ")
+            saldo = 0.00
+            nova_CC = CC(codigo, nome, saldo, "CC")
+            print(contas)
+            
+        case 2:
+            nome = input("Informe o nome do titular da conta: ")
+            saldo = 0.00
+            nova_CP = CP(codigo, nome, saldo, "CP")
+            print(contas)
         
+        case 3:
+            pass
+        
+        case 4:
+            pass
+        
+        case 5:
+            numero_conta = int(input("Qual o numero da conta que deseja verificar: "))
+            Contas.verificar_informacoes(numero_conta)
+            pass
+        
+        case 0:
+            print("Programa fechado!")
+            break
