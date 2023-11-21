34a6a5f56cff614903b8e606139b7a45a7ddd322
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2023-11-20 21:04:24 -0300

    Update ex02.py

--- python/aula13/ex02.py
+++ python/aula13/ex02.py
@@ -15,3 +15,83 @@ tipo.
 Lembre-se de utilizar todos os conceitos que vimos até
 agora! Utilize herança, polimorfismo e encapsulamento
 """
+from abc import ABC, abstractmethod
+from random import randint
+
+
+class Ingresso(ABC):
+    def __init__(self, identificador, preco, titulo_do_show):
+        self.identificador = identificador
+        self.__preco = preco
+        self.titulo_do_show = titulo_do_show
+
+    @property
+    def preco(self):
+        return self.__preco
+
+    def info(self):
+        print(f"Identificador: {self.identificador}\n"
+              f"Preço: {self.__preco}\n"
+              f"Titulo do Show: {self.titulo_do_show}")
+        
+
+    @abstractmethod
+    def tipo(self):
+        pass
+
+
+class Pista(Ingresso):
+
+    def tipo(self):
+        super().info()
+        print("Este ingresso é: pista\n")
+
+
+class Camarote(Ingresso):
+    def tipo(self):
+        super().info()
+        print("Este ingresso é: camarote\n")
+
+
+class VIPs(Ingresso):
+    def tipo(self):
+        super().info()
+        print("Este ingresso é: vip\n")
+
+
+input("Precione enter para começar!")
+ingressos = dict()
+identificador = 1
+while True:
+    print("1 - Ingresso para pista")
+    print("2 - Ingresso para camarote")
+    print("3 - Ingresso para Vip")
+    print("4 - Ver informações")
+    print("0 - Sair")
+    opcao = int(input("Escolha uma das opções acima: "))
+
+    match opcao:
+        case 1:
+            preco = randint(100,500) #float(input("Qual o preço do ingresso: "))
+            titulo_do_show = "Teste" #input("Qual o titulo do Show: ")
+            ingressos[identificador] = Pista(identificador, preco, titulo_do_show)
+            identificador += 1
+
+        case 2:
+            preco = randint(501,1000) #float(input("Qual o preço do ingresso: "))
+            titulo_do_show = "Teste" #input("Qual o titulo do Show: ")
+            ingressos[identificador] = Camarote(identificador, preco, titulo_do_show)
+            identificador += 1
+
+        case 3:
+            preco = randint(1001,2000) #float(input("Qual o preço do ingresso: "))
+            titulo_do_show = "Teste" #input("Qual o titulo do Show: ")
+            ingressos[identificador] = VIPs(identificador, preco, titulo_do_show)
+            identificador += 1
+        
+        case 4:
+            for i in ingressos:
+                ingressos[i].tipo()
+
+        case 0:
+            break
